5d1e488474a3a15848922902a3b6a3aaa6fb8359
William Banas <banaswilliam@gmail.com>
2024-08-16 17:22:30 +0200

    drag n drop done

---
+++ frontend/src/lib/components/Task.svelte
@@ -0,0 +1,39 @@
+<script context="module" lang="ts">
+	export interface TaskItem {
+		id: string;
+		title: string;
+		description: string;
+	}
+</script>
+
+<script lang="ts">
+	export let task: TaskItem;
+
+	let isOVerAnotherLi = false;
+  let isDragging = false
+
+  // class:after:top-[-7px]={isOVerAnotherLi}
+	// class:after:absolute={isOVerAnotherLi}
+	// class:after:w-full={isOVerAnotherLi}
+	// class:after:border-dashed={isOVerAnotherLi}
+	// class:after:border-t-2={isOVerAnotherLi}
+	// class:after:border-surface-100={isOVerAnotherLi}
+
+</script>
+
+<li
+	role="listitem"
+	id={task.id}
+	class="card min-h-12 flex items-center gap-2 px-2 relative transition"
+	draggable="true"
+	on:drag
+	on:dragenter={() => isOVerAnotherLi = true}
+	on:dragleave={() => isOVerAnotherLi = false}
+  on:drop={() => isOVerAnotherLi = false}
+  on:dragstart={() => isDragging = true}
+  on:dragend={() => isDragging = false}
+  class:opacity-50={isDragging}
+	 
+>
+	<p>{task.title}</p>
+</li>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-	import Header from '../lib/Header.svelte';
+	import Task, { type TaskItem } from '$lib/components/Task.svelte';
+	import Header from '../lib/components/Header.svelte';
 
-	const tasksTodo = [
+	let tasksTodo: TaskItem[] = [
 		{
 			id: '1',
 			title: 'Task 1',
@@ -17,29 +18,29 @@
 			title: 'Task 3',
 			description: 'This is my description'
 		},
-		{
-			id: '7',
-			title: 'Task 7',
-			description: 'This is my description'
-		},
-		{
-			id: '8',
-			title: 'Task 8',
-			description: 'This is my description'
-		},
-		{
-			id: '9',
-			title: 'Task 9',
-			description: 'This is my description'
-		},
-		{
-			id: '10',
-			title: 'Task 10',
-			description: 'This is my description'
-		}
+		// {
+		// 	id: '7',
+		// 	title: 'Task 7',
+		// 	description: 'This is my description'
+		// },
+		// {
+		// 	id: '8',
+		// 	title: 'Task 8',
+		// 	description: 'This is my description'
+		// },
+		// {
+		// 	id: '9',
+		// 	title: 'Task 9',
+		// 	description: 'This is my description'
+		// },
+		// {
+		// 	id: '10',
+		// 	title: 'Task 10',
+		// 	description: 'This is my description'
+		// }
 	];
 
-	const tasksThisWeek = [
+	let tasksThisWeek: TaskItem[] = [
 		{
 			id: '4',
 			title: 'Task 4',
@@ -47,7 +48,7 @@
 		}
 	];
 
-	const tasksToday = [
+	let tasksToday: TaskItem[] = [
 		{
 			id: '5',
 			title: 'Task 5',
@@ -55,7 +56,7 @@
 		}
 	];
 
-	const tasksDone = [
+	let tasksDone: TaskItem[] = [
 		{
 			id: '6',
 			title: 'Task 6',
@@ -68,133 +69,134 @@
 	let isDraggingOverToday = false;
 	let isDraggingOverDone = false;
 
-	function todoDragEnter() {
-		isDraggingOverTodo = true;
+	let taskDragging: TaskItem;
+
+	function drop(event: DragEvent, tasksList: TaskItem[]) {
+		const allTasks = [tasksToday, tasksTodo, tasksThisWeek, tasksDone];
+
+		allTasks.forEach((tasksArray) => {
+			const index = tasksArray.indexOf(taskDragging);
+			if (index > -1) {
+				tasksArray.splice(index, 1);
+			}
+		});
+
+		const dropTarget = event.target as HTMLElement;
+		if (dropTarget.nodeName === 'LI') {
+			const id = dropTarget.getAttribute('id');
+			const dropTargetTask = tasksList.find((task) => task.id === id);
+			if (dropTargetTask) {
+				const dropTargetIndex = tasksList.indexOf(dropTargetTask);
+				tasksList.splice(dropTargetIndex, 0, taskDragging);
+			}
+		} else {
+			tasksList.push(taskDragging);
+		}
+		tasksThisWeek = tasksThisWeek;
+		tasksTodo = tasksTodo;
+		tasksToday = tasksToday;
+		tasksDone = tasksDone;
 	}
 
-	function todoDragLeave() {
-		isDraggingOverTodo = false;
+	function dragging(event: DragEvent, tasksList: TaskItem[]) {
+		const id = (event.target as HTMLElement).getAttribute('id');
+		const task = tasksList.find((task) => task.id === id);
+		if (task) {
+			taskDragging = task;
+		}
 	}
 
-	function thisWeekDragEnter() {
-		isDraggingOverThisWeek = true;
+	function dragOver(event: DragEvent) {
+		event.preventDefault();
 	}
-
-	function thisWeekDragLeave() {
-		isDraggingOverThisWeek = false;
-	}
-
-	function todayDragEnter() {
-		isDraggingOverToday = true;
-	}
-
-	function todayDragLeave() {
-		isDraggingOverToday = false;
-	}
-
-	function doneDragEnter() {
-		isDraggingOverDone = true;
-	}
-
-	function doneDragLeave() {
-		isDraggingOverDone = false;
-	}
-
-	let todoArea: [];
-	let thisWeekArea: [];
-	let todayArea: [];
-	let doneArea: [];
 </script>
 
 <div class="h-full">
 	<Header />
 
 	<div class="flex gap-3 mx-auto mt-48 h-fit w-fit">
-
 		<div class="h-fit w-72 border-solid border-2 border-surface-600 rounded">
-			<div
-				class="h-16 bg-primary-600 rounded-tr rounded-tl border-b-2 border-surface-600 flex items-center justify-center"
-			>
+			<div class="h-16 bg-primary-600 rounded-tr rounded-tl flex items-center justify-center">
 				<p class="text-2xl">To do</p>
 			</div>
-			<div
+			<ul
 				role="group"
-				class="p-2 flex flex-col gap-2 h-80 overflow-y-auto"
+				class="p-4 flex flex-col gap-3 h-80 overflow-y-auto transition"
 				class:ring-2={isDraggingOverTodo}
 				class:ring-surface-100={isDraggingOverTodo}
 				class:ring-inset={isDraggingOverTodo}
-				on:dragenter={todoDragEnter}
-				on:dragleave={todoDragLeave}
+				on:dragenter={() => (isDraggingOverTodo = true)}
+				on:dragleave={() => (isDraggingOverTodo = false)}
+				on:drop={(event) => {drop(event, tasksTodo), isDraggingOverTodo = false}}
+				on:dragover={dragOver}
 			>
 				{#each tasksTodo as task}
-					<div id={task.id} class="card min-h-12 flex items-center gap-2 px-2" draggable="true">
-						<p>{task.title}</p>
-					</div>
+					<Task on:drag={(event) => dragging(event, tasksTodo)} {task} />
 				{/each}
-			</div>
+			</ul>
 		</div>
 
 		<div class="h-fit w-72 border-solid border-2 border-surface-600 rounded">
 			<div class="h-16 bg-error-600 rounded-tr rounded-tl flex items-center justify-center">
 				<p class="text-2xl">This week</p>
 			</div>
-			<div
+			<ul
 				role="group"
-				class="p-2 flex flex-col gap-2 h-80 overflow-y-auto"
+				class="p-4 flex flex-col gap-3 h-80 overflow-y-auto transition"
 				class:ring-2={isDraggingOverThisWeek}
 				class:ring-surface-100={isDraggingOverThisWeek}
 				class:ring-inset={isDraggingOverThisWeek}
-				on:dragenter={thisWeekDragEnter}
-				on:dragleave={thisWeekDragLeave}
+				on:dragenter={() => (isDraggingOverThisWeek = true)}
+				on:dragleave={() => (isDraggingOverThisWeek = false)}
+				on:drop={(event) => {drop(event, tasksThisWeek), isDraggingOverThisWeek = false}}
+				on:dragover={dragOver}
 			>
 				{#each tasksThisWeek as task}
-					<div id={task.id} class="card min-h-12 flex items-center gap-2 px-2" draggable="true">
-						<p>{task.title}</p>
-					</div>
+					<Task on:drag={(event) => dragging(event, tasksThisWeek)} {task} />
 				{/each}
-			</div>
+			</ul>
 		</div>
 
 		<div class="h-fit w-72 border-solid border-2 border-surface-600 rounded">
 			<div class="h-16 bg-warning-600 rounded-tr rounded-tl flex items-center justify-center">
 				<p class="text-2xl">Today</p>
 			</div>
-			<div
+			<ul
 				role="group"
-				class="p-2 flex flex-col gap-2 h-80 overflow-y-auto"
+				class="p-4 flex flex-col gap-3 h-80 overflow-y-auto transition"
 				class:ring-2={isDraggingOverToday}
 				class:ring-surface-100={isDraggingOverToday}
 				class:ring-inset={isDraggingOverToday}
-				on:dragenter={todayDragEnter}
-				on:dragleave={todayDragLeave}
+				on:dragenter={() => (isDraggingOverToday = true)}
+				on:dragleave={() => (isDraggingOverToday = false)}
+				on:drop={(event) => {drop(event, tasksToday), isDraggingOverToday = false}}
+				on:dragover={dragOver}
 			>
 				{#each tasksToday as task}
-					<div id={task.id} class="card min-h-12 flex items-center gap-2 px-2">
-						<p>{task.title}</p>
-					</div>
+					<Task on:drag={(event) => dragging(event, tasksToday)} {task} />
 				{/each}
-			</div>
+			</ul>
 		</div>
 
 		<div class="h-fit w-72 border-solid border-2 border-surface-600 rounded">
 			<div class="h-16 bg-success-600 rounded-tr rounded-tl flex items-center justify-center">
 				<p class="text-2xl">Done</p>
 			</div>
-			<div
+			<ul
 				role="group"
-				class="p-2 flex flex-col gap-2 h-80 overflow-y-auto"
+				class="p-4 flex flex-col gap-3 h-80 overflow-y-auto transition"
 				class:ring-2={isDraggingOverDone}
 				class:ring-surface-100={isDraggingOverDone}
 				class:ring-inset={isDraggingOverDone}
-				on:dragenter={doneDragEnter}
-				on:dragleave={doneDragLeave}
+				on:dragenter={() => (isDraggingOverDone = true)}
+				on:dragleave={() => (isDraggingOverDone = false)}
+				on:drop={(event) => {drop(event, tasksDone), isDraggingOverDone = false}}
+				on:dragover={dragOver}
 			>
 				{#each tasksDone as task}
-					<div id={task.id} class="card min-h-12 flex items-center gap-2 px-2" draggable="true">
-						<p>{task.title}</p>
-					</div>
+					<Task on:drag={(event) => dragging(event, tasksDone)} {task} />
 				{/each}
-			</div>
+			</ul>
 		</div>
 	</div>
 </div>
